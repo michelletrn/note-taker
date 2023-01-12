5ca6edf0c46d06c804fa5274408e87b65a734221
michelle <113802263+michelletrn@users.noreply.github.com>
2023-01-11 21:38:26 -0500

    added gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./db/db.json');
 const id = require('./helpers/uuid')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
